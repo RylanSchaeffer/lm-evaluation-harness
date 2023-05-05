c2dbad07d36d7cf066ba686a108c39017d4a9677
Matt-Harvill <m.harvill23@gmail.com>
2023-05-05 14:49:53 -0700

    top_k added

--- lm_eval/models/gpt2.py
+++ lm_eval/models/gpt2.py
@@ -116,7 +116,7 @@ class HFLM(BaseLM):
             return self.gpt2(inps)[0]
 
     def _model_generate(self, context, max_length, eos_token_id):
-        generation_kwargs = {'do_sample': False, 'max_length': max_length}
+        generation_kwargs = {'do_sample': True, 'max_length': max_length, 'top_k': 10}
         if eos_token_id is not None:
             generation_kwargs['eos_token_id'] = eos_token_id
         return self.gpt2.generate(context, **generation_kwargs)
