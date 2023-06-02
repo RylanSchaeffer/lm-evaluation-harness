583a0f337975e58df2d744461c5f02ba94b573a0
Matt-Harvill <m.harvill23@gmail.com>
2023-06-01 20:05:09 -0700

    added evaluate.load

--- lm_eval/tasks/truthfulqa.py
+++ lm_eval/tasks/truthfulqa.py
@@ -179,7 +179,7 @@ class TruthfulQAGeneration(Task):
                 "pip install bleurt@https://github.com/google-research/bleurt/archive/b610120347ef22b494b6d69b4316e303f5932516.zip#egg=bleurt"
                 "\nWARNING: Installing any other version of bleurt may result in different results."
             )
-        self.bleurt = evaluate.load_metric("bleurt")
+        self.bleurt = evaluate.load("bleurt")
 
     def has_training_docs(self):
         return False
